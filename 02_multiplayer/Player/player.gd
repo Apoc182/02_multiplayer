c87129df6427028e7fdfaf88ee605bf81319a32e
Alex Foster <alex.88.foster@gmail.com>
2023-09-07 10:52:14 +1000

    Convert input direction vector from cartesian to isometric

--- 02_multiplayer/Player/player.gd
+++ 02_multiplayer/Player/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED: int = 320
 
-@export var input_enabled: bool = true
+var input_enabled: bool = true
+var rotate_input_direction: bool = false
 
 @onready var health: Health = $Health
 
@@ -13,9 +14,22 @@ func is_this_client() -> bool:
 func _physics_process(_delta):
     if not is_this_client():
         return
+        
+    if Input.is_action_just_pressed("ui_accept"):
+        rotate_input_direction = !rotate_input_direction
+        
     var input_direction = get_input_direction()
-    velocity = input_direction.normalized() * SPEED
+    if rotate_input_direction:
+        input_direction = input_direction.rotated(deg_to_rad(315))
+    input_direction = cartesian_to_isometric(input_direction)
+
+    velocity = input_direction * SPEED
     move_and_slide()
+    
+    $Node/Label.global_position = Vector2(32, 32)
+    $Node/Label2.global_position = Vector2(32, 96)
+    $Node/Label2.text = "rotate_input_direction = " + str(rotate_input_direction)
+    
 
 func _ready():
     $MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
@@ -23,8 +37,9 @@ func _ready():
 func get_input_direction() -> Vector2:
     var input_direction_x = Input.get_axis("move_left", "move_right")
     var input_direction_y = Input.get_axis("move_up", "move_down")
+    
     if input_enabled:
-        return Vector2(input_direction_x, input_direction_y)
+        return Vector2(input_direction_x, input_direction_y).normalized()
     else:
         return Vector2.ZERO
 
@@ -42,3 +57,12 @@ func _on_health_changed(current_health):
     if current_health <= 0:
         print (name + " is dead")
         queue_free()
+
+
+func cartesian_to_isometric(cartesian_vector: Vector2) -> Vector2:
+    var isometric_vector = Vector2.ZERO
+
+    isometric_vector.x = cartesian_vector.x - cartesian_vector.y
+    isometric_vector.y = (cartesian_vector.x + cartesian_vector.y) / 2
+
+    return isometric_vector
